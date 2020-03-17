70116fc6dae7f43c70390b4424c27b6be8502b3b
Kirstine Nielsen <kirstine78@yahoo.com>
2020-03-17 20:13:42 +1100

    challenge Module "Lightning Data Service Basics for Aura Components" - Unit "Manipulate Records with force:recordData"

---
+++ src/aura/ldsDeleteRecord/ldsDeleteRecordController.js
@@ -0,0 +1,29 @@
+/**
+ * Created by Kirsti on 17/03/2020.
+ */
+
+({
+    handleDeleteRecord: function(component, event, helper) {
+    component.find("recordHandler").deleteRecord($A.getCallback(function(deleteResult) {
+        if (deleteResult.state === "SUCCESS" || deleteResult.state === "DRAFT") {
+            console.log("Record is deleted.");
+            var resultsToast = $A.get("e.force:showToast");
+            resultsToast.setParams({
+                "title": "Deleted",
+                "message": "The record was deleted."
+            });
+            resultsToast.fire();
+        }
+        else if (deleteResult.state === "INCOMPLETE") {
+            console.log("User is offline, device doesn't support drafts.");
+        }
+        else if (deleteResult.state === "ERROR") {
+            console.log('Problem deleting record, error: ' +
+                        JSON.stringify(deleteResult.error));
+        }
+        else {
+            console.log('Unknown problem, state: ' + deleteResult.state +
+                        ', error: ' + JSON.stringify(deleteResult.error));
+        }
+    }));
+}})

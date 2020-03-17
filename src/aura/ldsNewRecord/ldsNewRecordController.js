10f47bd6d417478d576108619c96da779d584f51
Kirstine Nielsen <kirstine78@yahoo.com>
2020-03-17 19:33:10 +1100

    add files

---
+++ src/aura/ldsNewRecord/ldsNewRecordController.js
@@ -0,0 +1,57 @@
+/**
+ * Created by Kirsti on 17/03/2020.
+ */
+
+({
+    doInit: function(component, event, helper) {
+        // Prepare a new record from template
+        component.find("contactRecordCreator").getNewRecord(
+            "Contact", // sObject type (entityAPIName)
+            null,      // recordTypeId
+            false,     // skip cache?
+            $A.getCallback(function() {
+                var rec = component.get("v.newContact");
+                var error = component.get("v.newContactError");
+                if(error || (rec === null)) {
+                    console.log("Error initializing record template: " + error);
+                }
+                else {
+                    console.log("Record template initialized: " + rec.apiName);
+                }
+            })
+        );
+    },
+
+    /*
+    Assuming you’re adding the ldsNewRecord custom component to an account record page, the v.recordId attribute
+    returns the record Id of the account record page where ldsNewRecord is placed.
+    The line component.set("v.simpleNewContact.AccountId", component.get("v.recordId")); sets the
+    account record Id on the AccountId field.
+    */
+    handleSaveContact: function(component, event, helper) {
+        if(helper.validateContactForm(component)) {
+            component.set("v.simpleNewContact.AccountId", component.get("v.recordId"));
+            component.find("contactRecordCreator").saveRecord(function(saveResult) {
+                if (saveResult.state === "SUCCESS" || saveResult.state === "DRAFT") {
+                    // record is saved successfully
+                    var resultsToast = $A.get("e.force:showToast");
+                    resultsToast.setParams({
+                        "title": "Saved",
+                        "message": "The record was saved."
+                    });
+                    resultsToast.fire();
+                } else if (saveResult.state === "INCOMPLETE") {
+                    // handle the incomplete state
+                    console.log("User is offline, device doesn't support drafts.");
+                } else if (saveResult.state === "ERROR") {
+                    // handle the error state
+                    console.log('Problem saving contact, error: ' +
+                                 JSON.stringify(saveResult.error));
+                } else {
+                    console.log('Unknown problem, state: ' + saveResult.state +
+                                ', error: ' + JSON.stringify(saveResult.error));
+                }
+            });
+        }
+    }
+})
